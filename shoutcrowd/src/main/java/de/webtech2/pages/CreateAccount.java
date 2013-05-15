383bb7be2b3da1f37044d9bc3411808b1d247252
Dennis Kuehn <dennis.kuehn@udo.edu>
2013-05-15 12:29:56 +0200

    CreateAccount ans Laufen gebracht

--- shoutcrowd/src/main/java/de/webtech2/pages/CreateAccount.java
+++ shoutcrowd/src/main/java/de/webtech2/pages/CreateAccount.java
@@ -1,34 +1,99 @@
 package de.webtech2.pages;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
 import org.apache.tapestry5.annotations.Component;
 import org.apache.tapestry5.annotations.InjectComponent;
 import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.corelib.components.Form;
 import org.apache.tapestry5.corelib.components.PasswordField;
-
+import org.apache.tapestry5.corelib.components.TextField;
+import org.apache.tapestry5.ioc.Messages;
+import org.apache.tapestry5.ioc.annotations.Inject;
 
 /**
  * Start page of application shoutcrowd.
  */
-public class CreateAccount
-{
+public class CreateAccount {
+	@Inject
+	Messages messages;
+	
 	@Property
-	String username;
+	private String email;
 	@Property
-	String password;
+	private String username;
 	@Property
-	String passwordRepeat;
+	private String password;
 	@Property
-	String email;
+	private String passwordRepeat;
 	
 	@Component
-	Form entryForm;
-	@InjectComponent(value="password")
+	private Form entryForm;
+	
+	@InjectComponent(value = "password")
 	private PasswordField passwordField;
+	@InjectComponent(value = "passwordRepeat")
+	private PasswordField passwordRepeatField;
+	@InjectComponent(value = "username")
+	private TextField usernameField;
+	@InjectComponent(value = "email")
+	private TextField emailField;
 
 	
 	void onValidateFromEntryForm() {
-		entryForm.recordError("noooo!");
+		
+		validatePassword();
+		validateEmail();
+		validateUsername();
 	}
 
+	private void validatePassword() {
+		if (password == null) {
+			entryForm.recordError(passwordField, messages.get("error-passwordtoshort"));
+		} else if (passwordRepeat == null) {
+			entryForm.recordError(passwordRepeatField, messages.get("error-passwordnotidentical"));
+		} else {
+			if (!password.equals(passwordRepeat)) {
+				entryForm.recordError(passwordRepeatField, messages.get("error-passwordnotidentical"));
+			}
+			if (password.length() < 6) {
+				entryForm.recordError(passwordField, messages.get("error-passwordtoshort"));
+			}
+		}
+	}
+	
+	private void validateUsername() {
+		if (username == null) {
+			entryForm.recordError(usernameField, messages.get("error-usernametoshort"));
+		} else {
+			if (username.length() < 4) {
+				entryForm.recordError(usernameField, messages.get("error-usernametoshort"));
+			}
+			// TODO: username already exists?
+		}
+	}
+
+	private void validateEmail() {
+		if (!isValidEmailAddress(email)) {
+			entryForm.recordError(emailField, messages.get("error-emailinvalid"));
+		} else {
+			// TODO: email already in use?
+		}
+	}
+
+	private boolean isValidEmailAddress(String email) {
+		boolean result = true;
+		try {
+			InternetAddress emailAddr = new InternetAddress(email);
+			emailAddr.validate();
+		} catch (AddressException ex) {
+			result = false;
+		}
+		return result;
+	}
+
+	private Object onSuccess() {
+		return Index.class;
+	}
 }
