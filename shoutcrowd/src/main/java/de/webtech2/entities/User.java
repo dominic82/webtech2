9f5d86f8f9ec64fa2112393c73a71e521065f535
Dennis Kuehn <dennis.kuehn@udo.edu>
2013-05-15 14:16:13 +0200

    Updated User Bean: added Getter&Setter for all attributes, added default costructor

--- shoutcrowd/src/main/java/de/webtech2/entities/User.java
+++ shoutcrowd/src/main/java/de/webtech2/entities/User.java
@@ -2,6 +2,8 @@ package de.webtech2.entities;
 
 import java.sql.Blob;
 import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.persistence.Entity;
@@ -16,6 +18,8 @@ import org.apache.tapestry5.beaneditor.NonVisual;
 @Entity
 public class User {
 
+	public final static Blob DEFAULT_AVATAR = null; // TODO: default value setzen
+	
 	@Id
 	@GeneratedValue(strategy=GenerationType.SEQUENCE)
 	@NonVisual
@@ -36,16 +40,136 @@ public class User {
 	private List<Message> messages;
 	
 	@ManyToMany
-	private List<User> FollowingUsers;
+	private List<User> followingUsers;
 	
 	@ManyToMany
-	private List<User> FollowedUsers;
+	private List<User> followedUsers;
 
 	@ManyToMany
-	private List<User> InvitedUsers;
+	private List<User> invitedUsers;
 	
 	@ManyToMany
-	private List<User> InvitingUsers; //(ManyToMany)
+	private List<User> invitingUsers; //(ManyToMany)
 
+	public User() {
+		timeCreated = new GregorianCalendar();
+		timeModified = timeCreated;
+		picture = DEFAULT_AVATAR;
+		
+		messages = new LinkedList<Message>();
+		followedUsers = new LinkedList<User>();
+		followingUsers = new LinkedList<User>();
+		invitedUsers = new LinkedList<User>();
+		invitingUsers = new LinkedList<User>();
+	}
 	
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return The MD5 sum of the password for this user.
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password The MD5 hashed password for this user.
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Blob getPicture() {
+		return picture;
+	}
+
+	public void setPicture(Blob picture) {
+		this.picture = picture;
+	}
+
+	public Calendar getTimeCreated() {
+		return timeCreated;
+	}
+
+	public void setTimeCreated(Calendar timeCreated) {
+		this.timeCreated = timeCreated;
+	}
+
+	public Calendar getTimeModified() {
+		return timeModified;
+	}
+
+	public void setTimeModified(Calendar timeModified) {
+		this.timeModified = timeModified;
+	}
+
+	public Calendar getTimeLastLogin() {
+		return timeLastLogin;
+	}
+
+	public void setTimeLastLogin(Calendar timeLastLogin) {
+		this.timeLastLogin = timeLastLogin;
+	}
+
+	public List<Message> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(List<Message> messages) {
+		this.messages = messages;
+	}
+
+	public List<User> getFollowingUsers() {
+		return followingUsers;
+	}
+
+	public void setFollowingUsers(List<User> followingUsers) {
+		this.followingUsers = followingUsers;
+	}
+
+	public List<User> getFollowedUsers() {
+		return followedUsers;
+	}
+
+	public void setFollowedUsers(List<User> followedUsers) {
+		this.followedUsers = followedUsers;
+	}
+
+	public List<User> getInvitedUsers() {
+		return invitedUsers;
+	}
+
+	public void setInvitedUsers(List<User> invitedUsers) {
+		this.invitedUsers = invitedUsers;
+	}
+
+	public List<User> getInvitingUsers() {
+		return invitingUsers;
+	}
+
+	public void setInvitingUsers(List<User> invitingUsers) {
+		this.invitingUsers = invitingUsers;
+	}
 }
