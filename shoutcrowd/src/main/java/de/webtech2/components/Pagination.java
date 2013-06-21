2e7c85b7c508b4e371f11be713c66afe82e823f3
Dominic Wirkner <dominic.wirkner@udo.edu>
2013-06-21 16:09:50 +0200

    Weitergemacht

--- shoutcrowd/src/main/java/de/webtech2/components/Pagination.java
+++ shoutcrowd/src/main/java/de/webtech2/components/Pagination.java
@@ -2,6 +2,7 @@
 package de.webtech2.components;
 
 import de.webtech2.util.PaginationItem;
+import java.util.ArrayList;
 import java.util.List;
 import org.apache.tapestry5.annotations.Parameter;
 import org.apache.tapestry5.annotations.Property;
@@ -17,19 +18,57 @@ public class Pagination {
     @Property
     private PaginationItem item;
     
+    @Property
     @Parameter(required = true)
     private int page;
     
+    @Property
     @Parameter(required = true)
     private int amount;
     
-    void onActivate() {
-        int start = ((this.page - 1) * this.amount);
-        if (start > 0) {
-            start--;
-        }
+    @Property
+    private int pageItem;
+    
+    private int maxPages;
+    
+    private void setupRender() {
+        int start = ((this.page - 1) * (this.amount - 1));
         int end = start + this.amount;
+        if (end > this.list.size()) {
+            end = this.list.size();
+        }
         this.subList = this.list.subList(start, end);
+        double tmpFloat = (double) this.list.size() / (double) amount;
+        this.maxPages = (int) Math.ceil(tmpFloat);
     }
     
+    public Integer getCount() {
+        return this.list.size();
+    }
+    
+    public Integer getSubCount() {
+        return this.subList.size();
+    }
+    
+    public List<Integer> getAllPages() {
+        List<Integer> pageList = new ArrayList<Integer>();
+        for (int i = 1; i <= this.maxPages; i++) {
+            pageList.add(new Integer(i));
+        }
+        return pageList;
+    }
+    
+    public boolean getPrevPossible() {
+        if (this.page - 1 <= 0) {
+            return false;
+        }
+        return true;
+    }
+    
+    public boolean getNextPossible() {
+        if (this.page - 1 >= this.maxPages - 1) {
+            return false;
+        }
+        return true;
+    }
 }
