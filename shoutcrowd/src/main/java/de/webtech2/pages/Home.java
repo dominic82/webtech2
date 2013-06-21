6ed3d6ce768c601bf11819c310461c86a7a9e26e
Dominic Wirkner <dominic.wirkner@udo.edu>
2013-06-21 13:21:16 +0200

    Pagination angefangen

--- shoutcrowd/src/main/java/de/webtech2/pages/Home.java
+++ shoutcrowd/src/main/java/de/webtech2/pages/Home.java
@@ -13,8 +13,17 @@ import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
 
 @RequiresLogin
-public class Home
-{
+public class Home {
+
+    private static final int PAGE_AMOUNT = 10;
+    @Property
+    private PageItem item;
+    @Property
+    private PageSource source;
+    @Inject
+    private PageItemService pageItemService;
+    private int page;
+    
     @Inject
     private UserDAO userDAO;
     @Inject
@@ -26,22 +35,36 @@ public class Home
     @Property
     private Message messageEntry;
 
-    void onActivate() {
+    void onActivate(int page) {
+        this.page = page;
         this.user = userDAO.getById(authenticator.getLoggedUser().getId());
     }
 
     private void setupRender() {
+
+        source = new PageSource(page, PAGE_AMOUNT) {
+            @Override
+            public Page loadPage(int start, int limit) {
+                return pageItemService.getPageItems(start, limit);
+            }
+
+            @Override
+            public Object[] getContext(int page) {
+                return new Object[]{page};
+            }
+        };
+
         List<Message> tmpList = new LinkedList<Message>();
         tmpList.addAll(this.user.getMessages());
         for (User tmpUser : this.user.getFollowingUsers()) {
             User followingUser = userDAO.getById(tmpUser.getId());
             tmpList.addAll(followingUser.getMessages());
-            
+
         }
         Collections.sort(tmpList, new CustomMessageDateComparator());
         this.messageList = tmpList;
     }
-    
+
     public boolean getIsLoggedUser() {
         if (authenticator.getLoggedUser().getId() == this.user.getId()) {
             return true;
@@ -50,3 +73,4 @@ public class Home
         }
     }
 }
+y
