da3a019d4a3561dc6345cf78067f5d9b4ee0b627
Muesdo <dominik.mues@gmx.de>
2013-06-21 14:32:33 +0200

    EditProfile Password validierung

--- shoutcrowd/src/main/java/de/webtech2/pages/EditProfile.java
+++ shoutcrowd/src/main/java/de/webtech2/pages/EditProfile.java
@@ -55,7 +55,7 @@ public class EditProfile {
     }
 
     private void validatePassword() {
-        if (password.equals("") && password != null) {
+        if (password != null) {
             if (!password.equals(passwordRepeat)) {
                 entryForm.recordError(passwordRepeatField, messages.get("error-passwordnotidentical"));
             }
